b72cb817fa5b636fefe2fd246b3f63e953cde402
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-19 15:38:13 -0700

    asdf

--- Fx.Games.Core.Tests/Db/System/Collections/Generic/ReadOnlyDictionaryExtensionsUnitTests.cs
+++ Fx.Games.Core.Tests/Db/System/Collections/Generic/ReadOnlyDictionaryExtensionsUnitTests.cs
@@ -8,5 +8,16 @@
     [TestClass]
     public sealed class ReadOnlyDictionaryExtensionsUnitTests
     {
+        /// <summary>
+        /// Retrieves a value from a <see langword="null"/> dictionary
+        /// </summary>
+        [TestMethod]
+        public void TryGetValueNullDictionary()
+        {
+            IReadOnlyDictionary<string, string> dictionary = null;
+            Assert.ThrowsException<global::System.ArgumentNullException>(() => 
+                dictionary
+                    .TryGetValue("a key", out var value));
+        }
     }
 }
