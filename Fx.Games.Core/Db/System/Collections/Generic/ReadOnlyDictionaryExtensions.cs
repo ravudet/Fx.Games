9429b379245182893e1520b2cf2ef8b7e9a79226
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-19 15:31:08 -0700

    asdf

--- Fx.Games.Core/Db/System/Collections/Generic/ReadOnlyDictionaryExtensions.cs
+++ Fx.Games.Core/Db/System/Collections/Generic/ReadOnlyDictionaryExtensions.cs
@@ -1,5 +1,8 @@
 ﻿namespace Db.System.Collections.Generic
 {
+    /// <summary>
+    /// Extension methods for <see cref="IReadOnlyDictionary{TKey, TValue}"/>
+    /// </summary>
     public static class ReadOnlyDictionaryExtensions
     {
         public static bool TryGetValue<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> self, TKey key, out TValue value)
