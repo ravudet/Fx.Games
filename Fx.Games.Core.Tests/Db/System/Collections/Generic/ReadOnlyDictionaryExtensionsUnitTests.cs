3d41d827b830f95f00478bcf500b85971874c59d
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-19 15:33:58 -0700

    asdf

---
+++ Fx.Games.Core.Tests/Db/System/Collections/Generic/ReadOnlyDictionaryExtensionsUnitTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Db.System.Collections.Generic
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for <see cref="ReadOnlyDictionaryExtensions"/>
+    /// </summary>
+    [TestClass]
+    public sealed class ReadOnlyDictionaryExtensionsUnitTests
+    {
+    }
+}
