fada25fb12ba0e69e0c1ae46c69131dd3df773a5
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-19 15:01:24 -0700

    asdf

--- Fx.Games.Core/Fx/Games/Driver/PlayerNotFoundExeption.cs
+++ Fx.Games.Core/Fx/Games/Driver/PlayerNotFoundExeption.cs
@@ -1,6 +1,7 @@
 ﻿namespace Fx.Games.Driver
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.Runtime.Serialization;
 
     /// <summary>
@@ -12,6 +13,7 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="PlayerNotFoundExeption"/> class.
         /// </summary>
+        [ExcludeFromCodeCoverage]
         public PlayerNotFoundExeption()
             : base()
         {
@@ -21,6 +23,7 @@
         /// Initializes an instance of the <see cref="PlayerNotFoundExeption"/> class with a specified error message.
         /// </summary>
         /// <param name="message">The error message that explains the reason for this exception.</param>
+        [ExcludeFromCodeCoverage]
         public PlayerNotFoundExeption(string message)
             : base(message)
         {
@@ -31,6 +34,7 @@
         /// </summary>
         /// <param name="message">The error message that explains the reason for this exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        [ExcludeFromCodeCoverage]
         public PlayerNotFoundExeption(string message, Exception innerException)
             : base(message, innerException)
         {
@@ -41,6 +45,7 @@
         /// </summary>
         /// <param name="info">The object that holds the serialized object data.</param>
         /// <param name="context">An object that describes the source or destination of the serialized data.</param>
+        [ExcludeFromCodeCoverage]
         public PlayerNotFoundExeption(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
