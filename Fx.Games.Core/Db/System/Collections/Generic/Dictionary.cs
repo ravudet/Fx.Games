5fd2d96380f73d7f2152626513db824f228a47d0
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-19 15:30:14 -0700

    asdf

--- Fx.Games.Core/Db/System/Collections/Generic/Dictionary.cs
+++ Fx.Games.Core/Db/System/Collections/Generic/Dictionary.cs
@@ -1,16 +1,35 @@
 ﻿namespace Db.System.Collections.Generic
 {
+    /// <summary>
+    /// A data structure that can look up values based on an associated key
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the collection</typeparam>
+    /// <typeparam name="TValue">The type of the values associated with the keys</typeparam>
     public sealed class Dictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
     {
+        /// <summary>
+        /// The data store for all of the key/value pairs where the key is not <see langword="null"/>
+        /// </summary>
         private readonly global::System.Collections.Generic.Dictionary<TKey, TValue> dictionary;
 
+        /// <summary>
+        /// The value (if any) for the <see langword="null"/> key
+        /// </summary>
         private Nullable<TValue> nullValue;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dictionary{TKey, TValue}"/> class
+        /// </summary>
         public Dictionary()
             : this(global::System.Collections.Generic.EqualityComparer<TKey>.Default)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dictionary{TKey, TValue}"/> class
+        /// </summary>
+        /// <param name="comparer">The <see cref="global::System.Collections.Generic.IEqualityComparer{T}"/> to use to compare the keys of the collection</param>
+        /// <exception cref="global::System.ArgumentNullException">Thrown if <paramref name="comparer"/> is <see langword="null"/></exception>
         public Dictionary(global::System.Collections.Generic.IEqualityComparer<TKey> comparer)
         {
             if (comparer == null)
@@ -22,6 +41,12 @@
             this.nullValue = default;
         }
 
+        /// <summary>
+        /// Adds <paramref name="value"/> to the collection associated with <paramref name="key"/>
+        /// </summary>
+        /// <param name="key">The key that is being added to the collection</param>
+        /// <param name="value">The value that is being added to the colleciton assocaited with <paramref name="key"/></param>
+        /// <exception cref="DuplicateKeyException">Thrown if <paramref name="key"/> is already present in the collection</exception>
         public void Add(TKey key, TValue value)
         {
             if (key == null)
