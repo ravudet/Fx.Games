40485bdb31d2c5c2f564f4d45f06b389672dd7d8
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-26 10:28:39 -0700

    asdf

--- Fx.Games.Core.Tests/Db/System/Collections/Generic/EnumerableExtensionsUnitTests.cs
+++ Fx.Games.Core.Tests/Db/System/Collections/Generic/EnumerableExtensionsUnitTests.cs
@@ -1,6 +1,9 @@
 ﻿namespace Db.System.Collections.Generic
 {
+    using DbAdapters.System.Collections.Generic;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Unit tests for <see cref="EnumerableExtensions"/>
@@ -14,8 +17,27 @@
         [TestMethod]
         public void ToDictionarySelectorsNullSequence()
         {
-            IEnumerable<global::System.Collections.Generic.KeyValuePair<string, string>> enumerable = null;
+            IEnumerable<string> enumerable = null;
             Assert.ThrowsException<global::System.ArgumentNullException>(() => enumerable.ToDictionary(_ => _, _ => _));
+        }
+
+        /// <summary>
+        /// Converts a sequence to a dictionary using a <see langword="null"/> key selector
+        /// </summary>
+        [TestMethod]
+        public void ToDictionarySelectorsNullKeySelector()
+        {
+            var enumerable = global::System.Linq.Enumerable.Empty<string>().ToDb();
+            global::System.Func<string, string> keySelector = null;
+            Assert.ThrowsException<global::System.ArgumentNullException>(() => enumerable.ToDictionary(keySelector, _ => _));
+        }
+
+        [TestMethod]
+        public void ToDictionarySelectorsNullValueSelector()
+        {
+            var enumerable = global::System.Linq.Enumerable.Empty<string>().ToDb();
+            global::System.Func<string, string> valueSelector = null;
+            Assert.ThrowsException<global::System.ArgumentNullException>(() => enumerable.ToDictionary(_ => _, valueSelector));
         }
     }
 }
