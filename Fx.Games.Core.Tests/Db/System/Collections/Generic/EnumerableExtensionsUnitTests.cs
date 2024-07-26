a07b2e878ebffe4d40457f605c74754e270195da
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-26 10:34:01 -0700

    asdf

--- Fx.Games.Core.Tests/Db/System/Collections/Generic/EnumerableExtensionsUnitTests.cs
+++ Fx.Games.Core.Tests/Db/System/Collections/Generic/EnumerableExtensionsUnitTests.cs
@@ -2,9 +2,7 @@
 {
     using DbAdapters.System.Collections.Generic;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using System;
-    using System.Collections.Generic;
-
+    
     /// <summary>
     /// Unit tests for <see cref="EnumerableExtensions"/>
     /// </summary>
@@ -32,12 +30,46 @@
             Assert.ThrowsException<global::System.ArgumentNullException>(() => enumerable.ToDictionary(keySelector, _ => _));
         }
 
+        /// <summary>
+        /// Converts a sequence to a dictionary using a <see langword="null"/> value selector
+        /// </summary>
         [TestMethod]
         public void ToDictionarySelectorsNullValueSelector()
         {
             var enumerable = global::System.Linq.Enumerable.Empty<string>().ToDb();
             global::System.Func<string, string> valueSelector = null;
             Assert.ThrowsException<global::System.ArgumentNullException>(() => enumerable.ToDictionary(_ => _, valueSelector));
+        }
+
+        /// <summary>
+        /// Converts a sequence to a dictionary
+        /// </summary>
+        [TestMethod]
+        public void ToDictionarySelectors()
+        {
+            var enumerable = new[] { "asdf", "12345677" }.ToDb();
+            var dictionary = enumerable.ToDictionary(element => element.Length, element => element);
+
+            bool contained;
+            string value;
+
+            value = dictionary.GetValueTry(4, out contained);
+            Assert.AreEqual(true, contained);
+            Assert.AreEqual("asdf", value);
+
+            value = dictionary.GetValueTry(8, out contained);
+            Assert.AreEqual(true, contained);
+            Assert.AreEqual("12345677", value);
+        }
+
+        /// <summary>
+        /// Converts a sequence to a dictionary when duplicate keys are generated
+        /// </summary>
+        [TestMethod]
+        public void ToDictionarySelectorsDuplicateKey()
+        {
+            var enumerable = new[] { "asdf", "qwer", "12345677" }.ToDb();
+            Assert.ThrowsException<DuplicateKeyException>(() => enumerable.ToDictionary(element => element.Length, element => element));
         }
     }
 }
