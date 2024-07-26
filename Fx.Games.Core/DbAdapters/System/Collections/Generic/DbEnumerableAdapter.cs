56f3e2cac500cc8dcfe2a8aed111a96bb72ecd6b
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-26 11:28:18 -0700

    asdf

--- Fx.Games.Core/DbAdapters/System/Collections/Generic/DbEnumerableAdapter.cs
+++ Fx.Games.Core/DbAdapters/System/Collections/Generic/DbEnumerableAdapter.cs
@@ -1,7 +1,7 @@
 ﻿namespace DbAdapters.System.Collections.Generic
 {
     /// <summary>
-    /// TODO use a struct? is boxing really "expensive" if the adapter ends up used as a reference? if you use a struct, the extension method should return the concrete type
+    /// 
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public sealed class DbEnumerableAdapter<T> : global::Db.System.Collections.Generic.IEnumerable<T>
@@ -31,8 +31,7 @@
                 return null; //// TODO is this the right behavior? what does foreach expect?
             }
 
-            return new DbEnumeratorAdapter<T>(cEnumerator); //// TODO should the constructor type parameterize the ienumerable so that you can get the struct variant of the enumerator?
-            //// TODO if you're doing all of these things with structs, shouldn't the be ref structs?
+            return new DbEnumeratorAdapter<T>(cEnumerator);
         }
 
         global::Db.System.Collections.Generic.IEnumerator<T> global::Db.System.Collections.Generic.IEnumerable<T>.GetEnumerator()
