02aec7241ad7c65fe574ce24c0aa5d78813a033e
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-26 10:23:52 -0700

    asdf

---
+++ Fx.Games.Core.Tests/Db/System/Collections/Generic/EnumerableExtensionsUnitTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Db.System.Collections.Generic
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for <see cref="EnumerableExtensions"/>
+    /// </summary>
+    [TestClass]
+    public sealed class EnumerableExtensionsUnitTests
+    {
+        /// <summary>
+        /// Converts a <see langword="null"/> sequence to a dictionary
+        /// </summary>
+        [TestMethod]
+        public void ToDictionarySelectorsNullSequence()
+        {
+            IEnumerable<global::System.Collections.Generic.KeyValuePair<string, string>> enumerable = null;
+            Assert.ThrowsException<global::System.ArgumentNullException>(() => enumerable.ToDictionary(_ => _, _ => _));
+        }
+    }
+}
--- Fx.Games.Core/Db/System/Collections/Generic/EnumerableExtensions.cs
+++ Fx.Games.Core/Db/System/Collections/Generic/EnumerableExtensions.cs
@@ -1,39 +1,20 @@
 ﻿namespace Db.System.Collections.Generic
 {
     /// <summary>
-    /// Extension methods for <see cref="global::System.Collections.Generic.IEnumerable{T}"/>
+    /// Extension methods for <see cref="IEnumerable{T}"/>
     /// </summary>
     public static class EnumerableExtensions
     {
         /// <summary>
-        /// TODO
+        /// Creates a <see cref="Dictionary{TKey, TValue}"/> from an <see cref="IEnumerable{T}"/> according to specified key selector and element selector functions.
         /// </summary>
-        /// <typeparam name="TKey"></typeparam>
-        /// <typeparam name="TValue"></typeparam>
-        /// <param name="self"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="self"/> is <see langword="null"/></exception>
-        /// <exception cref="DuplicateKeyException">Thrown if any key/value pair in <paramref name="self"/> has the same key as another key/value pair in <paramref name="self"/></exception>
-        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<global::System.Collections.Generic.KeyValuePair<TKey, TValue>> self)
-        {
-            if (self == null)
-            {
-                throw new global::System.ArgumentNullException(nameof(self));
-            }
-
-            return self.ToDictionary(keyValuePair => keyValuePair.Key, keyValuePair => keyValuePair.Value);
-        }
-
-        /// <summary>
-        /// TODO
-        /// </summary>
-        /// <typeparam name="TElement"></typeparam>
-        /// <typeparam name="TKey"></typeparam>
-        /// <typeparam name="TValue"></typeparam>
-        /// <param name="self"></param>
-        /// <param name="keySelector"></param>
-        /// <param name="valueSelector"></param>
-        /// <returns></returns>
+        /// <typeparam name="TElement">The type of the elements of <paramref name="self"/></typeparam>
+        /// <typeparam name="TKey">The type of the key returned by <paramref name="keySelector"/></typeparam>
+        /// <typeparam name="TValue">The type of the value returned by <paramref name="valueSelector"/></typeparam>
+        /// <param name="self">An <see cref="IEnumerable{T}"/> to create a <see cref="Dictionary{TKey, TValue}"/> from.</param>
+        /// <param name="keySelector">A function to extract a key from each element.</param>
+        /// <param name="valueSelector">A transform function to produce a result element value from each element.</param>
+        /// <returns>A <see cref="Dictionary{TKey, TValue}"/> that contains values of type <typeparamref name="TValue"/> selected from the input sequence.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="self"/> or <paramref name="keySelector"/> or <paramref name="valueSelector"/> is <see langword="null"/></exception>
         /// <exception cref="DuplicateKeyException">Thrown if any elements in <paramref name="self"/> contain duplicate keys as converted by <paramref name="keySelector"/></exception>
         public static Dictionary<TKey, TValue> ToDictionary<TElement, TKey, TValue>(this IEnumerable<TElement> self, global::System.Func<TElement, TKey> keySelector, global::System.Func<TElement, TValue> valueSelector)
@@ -62,6 +43,25 @@
             }
 
             return dictionary;
+        }
+
+        /// <summary>
+        /// Creates a dictionary from an enumeration according to the default comparer for the key type.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys from elements of <paramref name="self"/>.</typeparam>
+        /// <typeparam name="TValue">The type of the values from elements of <paramref name="self"/>.</typeparam>
+        /// <param name="self">The enumeration to create a dictionary from.</param>
+        /// <returns>A dictionary that contains keys and values from <paramref name="self"/> and uses the default comparer for the key type.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="self"/> is <see langword="null"/></exception>
+        /// <exception cref="DuplicateKeyException">Thrown if any key/value pair in <paramref name="self"/> has the same key as another key/value pair in <paramref name="self"/></exception>
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<global::System.Collections.Generic.KeyValuePair<TKey, TValue>> self)
+        {
+            if (self == null)
+            {
+                throw new global::System.ArgumentNullException(nameof(self));
+            }
+
+            return self.ToDictionary(keyValuePair => keyValuePair.Key, keyValuePair => keyValuePair.Value);
         }
     }
 }
