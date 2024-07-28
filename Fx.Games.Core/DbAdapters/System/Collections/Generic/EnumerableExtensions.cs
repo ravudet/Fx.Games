704334b21f39c72b3f5d2f9596a8e1c5b65998a5
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-28 10:02:19 -0700

    asdf

--- Fx.Games.Core/DbAdapters/System/Collections/Generic/EnumerableExtensions.cs
+++ Fx.Games.Core/DbAdapters/System/Collections/Generic/EnumerableExtensions.cs
@@ -1,16 +1,16 @@
 ﻿namespace DbAdapters.System.Collections.Generic
 {
     /// <summary>
-    /// TODO
+    /// Extension methods for <see cref="global::System.Collections.Generic.IEnumerable{T}"/> adapters to Db
     /// </summary>
     public static class EnumerableExtensions
     {
         /// <summary>
-        /// TODO
+        /// Converts a C# <see cref="global::System.Collections.Generic.IEnumerable{T}"/> into a Db <see cref="global::Db.System.Collections.Generic.IEnumerable{T}"/>
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="cEnumerable"></param>
-        /// <returns></returns>
+        /// <typeparam name="T">The type of the elements of the <see cref="global::Db.System.Collections.Generic.IEnumerable{T}"/></typeparam>
+        /// <param name="cEnumerable">The C# enumerable that should be adapted</param>
+        /// <returns>A Db enumerable that is adapted from <paramref name="cEnumerable"/></returns>
         /// <exception cref="global::System.ArgumentNullException">Thrown if <paramref name="cEnumerable"/> is <see langword="null"/></exception>
         public static global::Db.System.Collections.Generic.IEnumerable<T> ToDb<T>(this global::System.Collections.Generic.IEnumerable<T> cEnumerable)
         {
