485887bebc3b42c7d4ed21cba1a366488e19633d
ravudet <170215607+ravudet@users.noreply.github.com>
2024-07-28 09:29:42 -0700

    asdf

--- Fx.Games.Core/DbAdapters/System/Collections/Generic/DbEnumerableAdapter.cs
+++ Fx.Games.Core/DbAdapters/System/Collections/Generic/DbEnumerableAdapter.cs
@@ -1,17 +1,20 @@
 ﻿namespace DbAdapters.System.Collections.Generic
 {
     /// <summary>
-    /// 
+    /// An adapter from a C# <see cref="global::System.Collections.Generic.IEnumerable{T}"/> to a Db <see cref="Db.System.Collections.Generic.IEnumerable{T}"/>
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The type of the elements of the <see cref="Db.System.Collections.Generic.IEnumerable{T}"/></typeparam>
     public sealed class DbEnumerableAdapter<T> : global::Db.System.Collections.Generic.IEnumerable<T>
     {
+        /// <summary>
+        /// The C# enumerable that should be adapted
+        /// </summary>
         private readonly global::System.Collections.Generic.IEnumerable<T> cEnumerable;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="DbEnumerableAdapter{T}"/> class
         /// </summary>
-        /// <param name="cEnumerable"></param>
+        /// <param name="cEnumerable">The C# enumerable that should be adapted</param>
         /// <exception cref="global::System.ArgumentNullException">Thrown if <paramref name="cEnumerable"/> is <see langword="null"/></exception>
         public DbEnumerableAdapter(global::System.Collections.Generic.IEnumerable<T> cEnumerable)
         {
@@ -23,7 +26,8 @@
             this.cEnumerable = cEnumerable;
         }
 
-        public DbEnumeratorAdapter<T> GetEnumerator()
+        /// <inheritdoc/>
+        public global::Db.System.Collections.Generic.IEnumerator<T> GetEnumerator()
         {
             var cEnumerator = this.cEnumerable.GetEnumerator();
             if (cEnumerator == null)
@@ -32,11 +36,6 @@
             }
 
             return new DbEnumeratorAdapter<T>(cEnumerator);
-        }
-
-        global::Db.System.Collections.Generic.IEnumerator<T> global::Db.System.Collections.Generic.IEnumerable<T>.GetEnumerator()
-        {
-            return this.GetEnumerator();
         }
     }
 }
