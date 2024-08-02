61838c9ec4b470c177a9a1479b3072628e22038e
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-02 08:59:35 -0700

    added xmldoc for the driver unit tests

--- Fx.Games.Core.Tests/Fx/Games/Driver/DriverUnitTests.cs
+++ Fx.Games.Core.Tests/Fx/Games/Driver/DriverUnitTests.cs
@@ -119,6 +119,9 @@
             Assert.ThrowsException<ArgumentNullException>(() => driver.Run(null));
         }
 
+        /// <summary>
+        /// Runs a game that has a player who does not have a strategy configured in the driver
+        /// </summary>
         [TestMethod]
         public void PlayerNotFound()
         {
@@ -151,6 +154,9 @@
             var playerNotFoundException = Assert.ThrowsException<PlayerNotFoundExeption>(() => driver.Run(game));
         }
 
+        /// <summary>
+        /// Runs a game where strategies are actually leveraged to progress the game
+        /// </summary>
         [TestMethod]
         public void StrategyCalled()
         {
@@ -169,20 +175,32 @@
             Assert.AreEqual(game, calledGames[0]);
         }
 
+        /// <summary>
+        /// A mock implementation of <see cref="IGame{TGame, TBoard, TMove, TPlayer}"/> that plays a single turn with a single move
+        /// </summary>
         private sealed class MockGame : IGame<MockGame, string[], string, string>
         {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MockGame"/> class
+            /// </summary>
             public MockGame()
                 : this(false)
             {
             }
-            
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MockGame"/> class
+            /// </summary>
+            /// <param name="isGameOver">Whether the game should be considered over or not</param>
             private MockGame(bool isGameOver)
             {
                 this.IsGameOver = isGameOver;
             }
 
+            /// <inheritdoc/>
             public string CurrentPlayer => "asdf";
 
+            /// <inheritdoc/>
             public global::System.Collections.Generic.IEnumerable<string> Moves
             {
                 get
@@ -191,43 +209,79 @@
                 }
             }
 
+            /// <inheritdoc/>
             public string[] Board => throw new System.NotImplementedException();
 
+            /// <inheritdoc/>
             public WinnersAndLosers<string> WinnersAndLosers => throw new System.NotImplementedException();
 
+            /// <inheritdoc/>
             public bool IsGameOver { get; }
 
+            /// <inheritdoc/>
             public MockGame CommitMove(string move)
             {
                 return new MockGame(true);
             }
         }
 
+        /// <summary>
+        /// Factories for <see cref="MockStrategy{TGame, TBoard, TMove, TPlayer}"/>
+        /// </summary>
         private static class MockStrategy
         {
+            /// <summary>
+            /// Creates a new instance of <see cref="MockStrategy{TGame, TBoard, TMove, TPlayer}"/>
+            /// </summary>
+            /// <typeparam name="TGame">The type of the game that the new <see cref="MockStrategy{TGame, TBoard, TMove, TPlayer}"/> will play</typeparam>
+            /// <typeparam name="TBoard">The type of the board that the <typeparamref name="TGame"/> uses</typeparam>
+            /// <typeparam name="TMove">The type of the moves that the <typeparamref name="TGame"/> uses</typeparam>
+            /// <typeparam name="TPlayer">The type of the player that is playing the <typeparamref name="TGame"/></typeparam>
+            /// <param name="game">The game that the new <see cref="MockStrategy{TGame, TBoard, TMove, TPlayer}"/> will play</param>
+            /// <returns>A new instance of <see cref="MockStrategy{TGame, TBoard, TMove, TPlayer}"/></returns>
             public static MockStrategy<TGame, TBoard, TMove, TPlayer> Create<TGame, TBoard, TMove, TPlayer>(IGame<TGame, TBoard, TMove, TPlayer> game) where TGame : IGame<TGame, TBoard, TMove, TPlayer>
             {
                 return new MockStrategy<TGame, TBoard, TMove, TPlayer>();
             }
         }
 
+        /// <summary>
+        /// A mock implementation of <see cref="IStrategy{TGame, TBoard, TMove, TPlayer}"/> that always selects the first available move and keeps track of the games that it was asked to select a move for
+        /// </summary>
+        /// <typeparam name="TGame">The type of the game that the strategy can play</typeparam>
+        /// <typeparam name="TBoard">The type of the board that the <typeparamref name="TGame"/> uses</typeparam>
+        /// <typeparam name="TMove">The type of the moves that the <typeparamref name="TGame"/> uses</typeparam>
+        /// <typeparam name="TPlayer">The type of the player that is playing the <typeparamref name="TGame"/></typeparam>
         private sealed class MockStrategy<TGame, TBoard, TMove, TPlayer> : IStrategy<TGame, TBoard, TMove, TPlayer> where TGame : IGame<TGame, TBoard, TMove, TPlayer>
         {
+            /// <summary>
+            /// The games that this strategy has been asked to select a move for
+            /// </summary>
             private readonly List<TGame> calledGames;
 
+            /// <summary>
+            /// Initializes a new instance of the <see cref="MockStrategy{TGame, TBoard, TMove, TPlayer}"/> class
+            /// </summary>
             public MockStrategy()
             {
                 this.calledGames = new List<TGame>();
             }
 
+            /// <summary>
+            /// The games that this strategy has been asked to select a move for
+            /// </summary>
             public global::System.Collections.Generic.IEnumerable<TGame> CalledGames
             {
                 get
                 {
-                    return this.calledGames;
+                    foreach (var element in this.calledGames)
+                    {
+                        yield return element;
+                    }
                 }
             }
 
+            /// <inheritdoc/>
             public TMove SelectMove(TGame game)
             {
                 this.calledGames.Add(game);
