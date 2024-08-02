400116e0c3ee84c817bb4d45b828831071437cd4
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-02 09:57:34 -0700

    productized randomstrategy (#5)

---
+++ Fx.Games.Core.Tests/Fx/Games/Strategy/RandomStrategyUnitTests.cs
@@ -0,0 +1,160 @@
+﻿namespace Fx.Games.Strategy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Fx.Games.Game;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for <see cref="RandomStrategy{TGame, TBoard, TMove, TPlayer}"/>
+    /// </summary>
+    [TestClass]
+    public sealed class RandomStrategyUnitTests
+    {
+        /// <summary>
+        /// Creates a <see cref="RandomStrategy{TGame, TBoard, TMove, TPlayer}"/> using <see langword="null"/> settings
+        /// </summary>
+        [TestMethod]
+        public void NullSettings()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new RandomStrategy<MockGame, string[], string, string>(null));
+        }
+
+        /// <summary>
+        /// Selects a move from a <see langword="null"/> game
+        /// </summary>
+        [TestMethod]
+        public void SelectMoveNullGame()
+        {
+            var strategy = new RandomStrategy<MockGame, string[], string, string>();
+
+            Assert.ThrowsException<ArgumentNullException>(() => strategy.SelectMove(null));
+        }
+
+        /// <summary>
+        /// Selects a move from a game with no moves
+        /// </summary>
+        [TestMethod]
+        public void SelectMoveNoMoves()
+        {
+            var strategy = new RandomStrategy<MovelessGame, string[], string, string>();
+            var game = new MovelessGame();
+
+            Assert.ThrowsException<InvalidGameException>(() => strategy.SelectMove(game));
+        }
+
+        /// <summary>
+        /// Selects a move from a game
+        /// </summary>
+        [TestMethod]
+        public void SelectMove()
+        {
+            var strategy = new RandomStrategy<SingleMoveGame, string[], string, string>();
+            var game = new SingleMoveGame();
+
+            var move = strategy.SelectMove(game);
+
+            Assert.AreEqual("asdf", move);
+        }
+
+        /// <summary>
+        /// A <see cref="IGame{TGame, TBoard, TMove, TPlayer}"/> that has a single legal move
+        /// </summary>
+        private sealed class SingleMoveGame : IGame<SingleMoveGame, string[], string, string>
+        {
+            /// <summary>
+            /// The single move that this game always has available
+            /// </summary>
+            private readonly IEnumerable<string> moves = Enumerable.Repeat("asdf", 1);
+
+            /// <inheritdoc/>
+            public string CurrentPlayer => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public IEnumerable<string> Moves
+            {
+                get
+                {
+                    return this.moves;
+                }
+            }
+
+            /// <inheritdoc/>
+            public string[] Board => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public WinnersAndLosers<string> WinnersAndLosers => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public bool IsGameOver => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public SingleMoveGame CommitMove(string move)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// A <see cref="IGame{TGame, TBoard, TMove, TPlayer}"/> that never has any legal moves
+        /// </summary>
+        private sealed class MovelessGame : IGame<MovelessGame, string[], string, string>
+        {
+            /// <inheritdoc/>
+            public string CurrentPlayer => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public IEnumerable<string> Moves
+            {
+                get
+                {
+                    return Enumerable.Empty<string>();
+                }
+            }
+
+            /// <inheritdoc/>
+            public string[] Board => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public WinnersAndLosers<string> WinnersAndLosers => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public bool IsGameOver => throw new NotImplementedException();
+
+            /// <inheritdoc/>
+            public MovelessGame CommitMove(string move)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// A mock implementation of <see cref="IGame{TGame, TBoard, TMove, TPlayer}"/> that is not implemented
+        /// </summary>
+        private sealed class MockGame : IGame<MockGame, string[], string, string>
+        {
+            /// <inheritdoc/>
+            public string CurrentPlayer => throw new System.NotImplementedException();
+
+            /// <inheritdoc/>
+            public IEnumerable<string> Moves => throw new System.NotImplementedException();
+
+            /// <inheritdoc/>
+            public string[] Board => throw new System.NotImplementedException();
+
+            /// <inheritdoc/>
+            public WinnersAndLosers<string> WinnersAndLosers => throw new System.NotImplementedException();
+
+            /// <inheritdoc/>
+            public bool IsGameOver => throw new System.NotImplementedException();
+
+            /// <inheritdoc/>
+            public MockGame CommitMove(string move)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
