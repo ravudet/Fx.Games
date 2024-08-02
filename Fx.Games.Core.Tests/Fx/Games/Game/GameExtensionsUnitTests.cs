7d6dc4cd5c3551f20c4a83951f8de6b76f717ca8
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-02 16:02:59 -0700

    added unit tests for the gameextensions

---
+++ Fx.Games.Core.Tests/Fx/Games/Game/GameExtensionsUnitTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Fx.Games.Game
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for <see cref="GameExtensions"/>
+    /// </summary>
+    [TestClass]
+    public sealed class GameExtensionsUnitTests
+    {
+        /// <summary>
+        /// Gets the random strategy for a game
+        /// </summary>
+        [TestMethod]
+        public void RandomStrategy()
+        {
+            var strategy = NoImplementationGame.Instance.RandomStrategy();
+
+            Assert.IsNotNull(strategy);
+        }
+
+        /// <summary>
+        /// Gets the console strategy for a game
+        /// </summary>
+        [TestMethod]
+        public void ConsoleStrategy()
+        {
+            var strategy = NoImplementationGame.Instance.ConsoleStrategy();
+
+            Assert.IsNotNull(strategy);
+        }
+    }
+}
