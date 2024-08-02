e23c7f35e40c9b556457e499bb9fd806a021ccca
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-02 10:08:55 -0700

    productized the randomstrategy factory method

--- Fx.Games.Core/Fx/Games/Game/GameExtensions.cs
+++ Fx.Games.Core/Fx/Games/Game/GameExtensions.cs
@@ -2,6 +2,7 @@
 {
     using Fx.Games.Displayer;
     using Fx.Games.Driver;
+    using Fx.Games.Strategy;
 
     /// <summary>
     /// Extension methods for <see cref="IGame{TGame, TBoard, TMove, TPlayer}"/>
@@ -29,11 +30,25 @@
         /// <typeparam name="TBoard">The type of the board that <typeparamref name="TGame"/> uses</typeparam>
         /// <typeparam name="TMove">The type of the moves that the <typeparamref name="TGame"/> uses</typeparam>
         /// <typeparam name="TPlayer">The type of the player that is playing the <typeparamref name="TGame"/></typeparam>
-        /// <param name="game">The game that the <see cref="Driver{TGame, TBoard, TMove, TPlayer}"/> will player</param>
+        /// <param name="game">The game that the <see cref="Driver{TGame, TBoard, TMove, TPlayer}"/> will play</param>
         /// <returns>A builder instance for the <see cref="DriverSettings{TGame, TBoard, TMove, TPlayer}(IGame{TGame, TBoard, TMove, TPlayer})"/> that would be used to configure a <see cref="Driver{TGame, TBoard, TMove, TPlayer}"/> that plays <paramref name="game"/></returns>
         public static DriverSettings<TGame, TBoard, TMove, TPlayer>.Builder DriverSettings<TGame, TBoard, TMove, TPlayer>(this IGame<TGame, TBoard, TMove, TPlayer> game) where TGame : IGame<TGame, TBoard, TMove, TPlayer>
         {
             return new DriverSettings<TGame, TBoard, TMove, TPlayer>.Builder();
+        }
+
+        /// <summary>
+        /// Gets a <see cref="Fx.Games.Strategy.RandomStrategy{TGame, TBoard, TMove, TPlayer}"/> that can be used to play <paramref name="self"/>
+        /// </summary>
+        /// <typeparam name="TGame">The type of the game that the <see cref="Fx.Games.Strategy.RandomStrategy{TGame, TBoard, TMove, TPlayer}"/> would play</typeparam>
+        /// <typeparam name="TBoard">The type of the board that <typeparamref name="TGame"/> uses</typeparam>
+        /// <typeparam name="TMove">The type of the moves that the <typeparamref name="TGame"/> uses</typeparam>
+        /// <typeparam name="TPlayer">The type of the player that is playing the <typeparamref name="TGame"/></typeparam>
+        /// <param name="self">The game that the <see cref="Fx.Games.Strategy.RandomStrategy{TGame, TBoard, TMove, TPlayer}"/> will play</param>
+        /// <returns>A <see cref="Fx.Games.Strategy.RandomStrategy{TGame, TBoard, TMove, TPlayer}"/> that can be used to play <paramref name="self"/></returns>
+        public static RandomStrategy<TGame, TBoard, TMove, TPlayer> RandomStrategy<TGame, TBoard, TMove, TPlayer>(this IGame<TGame, TBoard, TMove, TPlayer> self) where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+        {
+            return new RandomStrategy<TGame, TBoard, TMove, TPlayer>();
         }
     }
 }
