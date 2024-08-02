bf9eea8523ec6e5d3c638b67168a1c40ea0b3c3f
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-02 15:52:11 -0700

    moved the consolelock to a new shared testcore project (#7)

---
+++ Fx.Games.TestCore/System/ConsoleUtilities.cs
@@ -0,0 +1,13 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Utilites for the <see cref="Console"/> class
+    /// </summary>
+    public static class ConsoleUtilities
+    {
+        /// <summary>
+        /// An object used to lock <see cref="Console"/> input, output, and redirect readers and writers for thread-safety purposes
+        /// </summary>
+        public static object ConsoleLock { get; } = new object();
+    }
+}
