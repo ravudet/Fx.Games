bc2d66ad8dab9be6dd5838bda5664638fcf2f342
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-04 11:42:20 -0700

    productizing the peg game

---
+++ Fx.Games.Core.Tests/Fx/Games/Game/PegPositionUnitTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Fx.Games.Game
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    /// <summary>
+    /// Unit tests for <see cref="PegPosition"/>
+    /// </summary>
+    [TestClass]
+    public sealed class PegPositionUnitTests
+    {
+        /// <summary>
+        /// Creates a peg position with a negative row
+        /// </summary>
+        [TestMethod]
+        public void NegativeRow()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new PegPosition(-1, 3));
+        }
+
+        /// <summary>
+        /// Creates a peg position with a row beyond the triangle
+        /// </summary>
+        [TestMethod]
+        public void LargeRow()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new PegPosition(5, 3));
+        }
+
+        /// <summary>
+        /// Creates a peg position with a negative column
+        /// </summary>
+        [TestMethod]
+        public void NegativeColumn()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new PegPosition(0, -1));
+        }
+
+        /// <summary>
+        /// Creates a peg position with a column beyond the triangle
+        /// </summary>
+        [TestMethod]
+        public void LargeColumn()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new PegPosition(3, 4));
+        }
+
+        /// <summary>
+        /// Creates a peg position that is within the triangle
+        /// </summary>
+        [TestMethod]
+        public void WithinTriangle()
+        {
+            var pegPosition = new PegPosition(3, 2);
+
+            Assert.AreEqual(3, pegPosition.Row);
+            Assert.AreEqual(2, pegPosition.Column);
+        }
+    }
+}
---
+++ Fx.Games.Core/Fx/Games/Game/PegMove.cs
@@ -0,0 +1,42 @@
+﻿namespace Fx.Games.Game
+{
+    using System;
+
+    /// <summary>
+    /// The movement of a <see cref="Peg"/> from one <see cref="PegPosition"/> to another
+    /// </summary>
+    public sealed class PegMove
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PegMove"/> class
+        /// </summary>
+        /// <param name="start">The position that the peg starts at</param>
+        /// <param name="end">The position that the peg should be moved to</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="start"/> or <paramref name="end"/> is <see langword="null"/></exception>
+        public PegMove(PegPosition start, PegPosition end)
+        {
+            if (start == null)
+            {
+                throw new ArgumentNullException(nameof(start));
+            }
+
+            if (end == null)
+            {
+                throw new ArgumentNullException(nameof(end));
+            }
+
+            this.Start = start;
+            this.End = end;
+        }
+
+        /// <summary>
+        /// The position that the peg starts at
+        /// </summary>
+        public PegPosition Start { get; }
+
+        /// <summary>
+        /// The position that the peg should be moved to
+        /// </summary>
+        public PegPosition End { get; }
+    }
+}
