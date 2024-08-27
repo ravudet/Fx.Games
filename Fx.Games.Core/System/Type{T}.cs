e433b88f1c377ec17490340c8e95d21731c50589
ravudet <170215607+ravudet@users.noreply.github.com>
2024-08-27 10:46:46 -0700

    driver from type<T>

---
+++ Fx.Games.Core/System/Type{T}.cs
@@ -0,0 +1,55 @@
+﻿using Fx.Games.Driver;
+using Fx.Games.Game;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
+
+namespace System
+{
+    public static class Utilities
+    {
+        public static IType<T> Type<T>()
+        {
+            return new Type<T>(typeof(T));
+        }
+    }
+
+    public static class TypeExtensions
+    {
+        public static Driver<TGame, TBoard, TMove, TPlayer> Driver<TGame, TBoard, TMove, TPlayer>(this IType<IGame<TGame, TBoard, TMove, TPlayer>> type) where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+        {
+            return new Driver<TGame, TBoard, TMove, TPlayer>(null, null, null);
+        }
+    }
+
+    public static class Main
+    {
+        public static void DoWork()
+        {
+            Utilities.Type<TicTacToe<string>>().Driver();
+        }
+    }
+
+    public interface IType<out T>
+    {
+        Type Typed { get; }
+    }
+
+    public sealed class Type<T> : IType<T>
+    {
+        private readonly Type type;
+
+        public Type(Type type)
+        {
+            this.type = type;
+        }
+
+        public Type Typed
+        {
+            get
+            {
+                return this.type;
+            }
+        }
+    }
+}
