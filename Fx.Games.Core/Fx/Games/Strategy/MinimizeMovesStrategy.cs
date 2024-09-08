254445f306020b87eaf5dfee4d99a0e59eaf4ae6
ravudet <170215607+ravudet@users.noreply.github.com>
2024-09-08 10:27:55 -0700

    adding a strategy to minimize the number of opponent moves and then added amazons variants with monte carlo and minimize moves

---
+++ Fx.Games.Core/Fx/Games/Strategy/MinimizeMovesStrategy.cs
@@ -0,0 +1,26 @@
+﻿namespace Fx.Games.Strategy
+{
+    using Fx.Games.Game;
+    using System.Linq;
+
+    public sealed class MinimizeMovesStrategy<TGame, TBoard, TMove, TPlayer> : IStrategy<TGame, TBoard, TMove, TPlayer> where TGame : IGame<TGame, TBoard, TMove, TPlayer>
+    {
+        private MinimizeMovesStrategy()
+        {
+        }
+
+        public static MinimizeMovesStrategy<TGame, TBoard, TMove, TPlayer> Instance { get; } = new MinimizeMovesStrategy<TGame, TBoard, TMove, TPlayer>();
+
+        public TMove SelectMove(TGame game)
+        {
+            var moves = game.Moves.ToList();
+            if (moves.Count == 0)
+            {
+                throw new InvalidGameException("tODO");
+            }
+
+            //// TODO we already assert that there are moves
+            return moves.MinBy(move => game.CommitMove(move).Moves.Count())!;
+        }
+    }
+}
