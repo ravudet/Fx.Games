ca5ad63d038d5dea654caa78bb7af229fb0478ef
ravudet <170215607+ravudet@users.noreply.github.com>
2024-12-16 16:05:37 -0800

    asdf

--- Fx.Games.Core/Fx/Distribution/WeightedDistribution.cs
+++ Fx.Games.Core/Fx/Distribution/WeightedDistribution.cs
@@ -8,6 +8,8 @@
 
         public WeightedDistribution(IDistribution<int> uniformDistribution, PortionV2<TValue> portions)
         {
+            //// TODO you toyed around with `sample` taking in a `idistribution` parameter so that distributions could be composed; in this constructor, you are composing a weighted distribution with a uniform distribution, but you've recognized in the comment below that allowed a `idistribution` in the constructor means that any caller of `weighteddistribution` may be properly assuming a weighted distribution, but they are really getting some composition (particularly if they take a look at the `portions` property); maybe what makes the most sense is `idistribution` having a `idistribution compose(idistribution)` method for the compositions, and `weighteddistribution` could take a `uniformdistribution` in as a parameter
+
             this.uniformDistribution = uniformDistribution; //// TODO for this class to really honor the weights, this needs to be a uniform distribution, but requiring specifically a uniform distribution removes composability; what do you want to do here? should there be a `compose` method that takes a new distribution or something?
             this.Portions = portions;
         }
