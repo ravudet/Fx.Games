9687b00e1943cefe83331347bfcd65135cea0107
ravudet <170215607+ravudet@users.noreply.github.com>
2024-12-27 12:56:24 -0800

    asfd

---
+++ Fx.Games.Core/Fx/Games/Game/Battleship.cs
@@ -0,0 +1,51 @@
+﻿using Fx.Distribution;
+using System.Collections.Generic;
+
+namespace Fx.Games.Game
+{
+    public sealed class Battleship<TPlayer> : IGame<Battleship<TPlayer>, BattleshipBoard, BattleshipMove, TPlayer, WeightedDistribution<Battleship<TPlayer>>>
+    {
+        private readonly (TPlayer Player, BattleshipBoard Board) current;
+
+        private readonly (TPlayer Player, BattleshipBoard Board) other;
+
+        public Battleship(TPlayer first, TPlayer second)
+        {
+            this.current = (first, new BattleshipBoard.Placement());
+            this.other = (second, new BattleshipBoard.Placement());
+        }
+
+        public TPlayer CurrentPlayer
+        {
+            get
+            {
+                return this.current.Player;
+            }
+        }
+
+        public BattleshipBoard Board
+        {
+            get
+            {
+                //// TODO having multiple derived types for battleshipboard may make it difficult to call this property...
+                return this.current.Board;
+            }
+        }
+
+        public WinnersAndLosers<TPlayer> WinnersAndLosers => throw new System.NotImplementedException();
+
+        public bool IsGameOver => throw new System.NotImplementedException();
+
+        IEnumerable<BattleshipMove> IGame<Battleship<TPlayer>, BattleshipBoard, BattleshipMove, TPlayer, WeightedDistribution<Battleship<TPlayer>>>.Moves => throw new System.NotImplementedException();
+
+        Battleship<TPlayer> IGame<Battleship<TPlayer>, BattleshipBoard, BattleshipMove, TPlayer, WeightedDistribution<Battleship<TPlayer>>>.CommitMove(BattleshipMove move)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        WeightedDistribution<Battleship<TPlayer>> IGame<Battleship<TPlayer>, BattleshipBoard, BattleshipMove, TPlayer, WeightedDistribution<Battleship<TPlayer>>>.ExploreMove(BattleshipMove move)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Fx.Games.Core/Fx/Games/Game/BattleshipBoard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fx.Games.Game
+{
+    public abstract class BattleshipBoard
+    {
+        private BattleshipBoard()
+        {
+        }
+
+        public sealed class Placement : BattleshipBoard
+        {
+            public Placement()
+            {
+            }
+
+            public Placement Place(Ship ship, (int X, int Y) location, Direction direction)
+            {
+            }
+
+            public Discovery Complete()
+            {
+            }
+        }
+
+        public sealed class Discovery : BattleshipBoard
+        {
+        }
+    }
+
+    public abstract class Direction
+    {
+        private Direction()
+        {
+        }
+    }
+
+    public abstract class Ship
+    {
+        private Ship()
+        {
+        }
+    }
+}
