1a724c24f9b51af62125fac3f9cfe38ab99cd684
ravudet <170215607+ravudet@users.noreply.github.com>
2025-02-22 17:31:14 -0800

    asdf

--- Fx.Games.Core/Fx/Interval/Interval.cs
+++ Fx.Games.Core/Fx/Interval/Interval.cs
@@ -15,14 +15,14 @@
 #pragma warning restore anidTODO // Type or member is obsolete
         }
 
-        public static Interval<TMinimum, TIntermediate, TNumeric, TValue>.Partition<TIntermediate, TMaximum, TSubInterval> Partition<TMinimum, TIntermediate, TNumeric, TValue, TMaximum, TSubInterval>(TSubInterval subInterval, TMaximum maximum, TValue value)
+        public static Interval<TMinimum, TIntermediate, TNumeric, TValue>.Partition<TMaximum, TSubInterval> Partition<TMinimum, TIntermediate, TNumeric, TValue, TMaximum, TSubInterval>(TSubInterval subInterval, TMaximum maximum, TValue value)
             where TMinimum : TNumeric
             where TIntermediate : TNumeric, IGreaterThan<TMinimum>
             where TMaximum : TNumeric, IGreaterThan<TIntermediate>, IGreaterThan<TMinimum>
             where TSubInterval : Interval<TMinimum, TIntermediate, TNumeric, TValue>
         {
 #pragma warning disable anotheridTODO // Type or member is obsolete
-            return new Interval<TMinimum, TIntermediate, TNumeric, TValue>.Partition<TIntermediate, TMaximum, TSubInterval>(subInterval, maximum, value);
+            return new Interval<TMinimum, TIntermediate, TNumeric, TValue>.Partition<TMaximum, TSubInterval>(subInterval, maximum, value);
 #pragma warning restore anotheridTODO // Type or member is obsolete
         }
     }
